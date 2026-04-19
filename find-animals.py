204d208cd83e52281fb9f930b9e3f930f5829e46
Quentin Rivest <queriv47x@csu.fullerton.edu>
2026-04-19 07:52:31 -0700

    Added find-animals file with function to find list of animals in an area given a latitude and longitude.

---
+++ find-animals.py
@@ -0,0 +1,46 @@
+import math
+import string
+
+from pyobis import occurrences
+
+
+# Makes octagon polygon (to mimic circle). [HELPER FOR getScinamesNamesDepth]
+def getWKTPolygonStr(lat: float, long: float, radius: float) -> str:
+  return (f"POLYGON(({long - 0.7*radius} {lat + radius}, {long + 0.7*radius} {lat + radius}, " +  #top side
+          f"{long + radius} {lat + 0.7*radius}, {long + radius} {lat - 0.7*radius}, " +  # right side
+          f"{long - 0.7*radius} {lat - radius}, {long + 0.7*radius} {lat - radius}, " +  # bottom side
+          f"{long - radius} {lat + 0.7*radius}, {long - radius} {lat - 0.7*radius}, " +  # left side
+          f"{long - 0.7*radius} {lat + radius}))")  # repeat of first coord to connect polygon
+
+
+# Sorted by depth (in meters), least to greatest.
+def getScinamesNamesDepths(*,
+                           lat: float,
+                           long: float,
+                           radius: float=5) -> list[tuple[str, str, float]]:
+  data = occurrences.search(
+    taxonid=2,
+    geometry=getWKTPolygonStr(lat, long, radius),
+    size=50
+  ).execute()
+
+  scinames_names_depths = []
+
+  for sci_name, name, depth in zip(data["scientificName"], data["vernacularName"], data["depth"]):
+    if isinstance(name, str) and not math.isnan(depth):
+      scinames_names_depths.append( (string.capwords(sci_name), string.capwords(name), depth) )
+
+  scinames_names_depths.sort(
+    key=lambda sciname_name_depth : sciname_name_depth[2]
+  )
+
+  return scinames_names_depths
+
+
+def printScinamesNamesDepth() -> None:
+  print("ANIMAL NAMES:")
+  for sci_name, name, depth in getScinamesNamesDepths(lat=32, long=-118):
+    print(f"\t{name} ({sci_name})\t{depth}")
+
+
+# printScinamesNamesDepth()
