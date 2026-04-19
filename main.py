e2b696573581fc20ca73944824508aac5f9336c5
Evan Jimenez <evanmji1123@csu.fullerton.edu>
2026-04-19 15:44:21 -0700

    setting up json conversion

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def dive():
     lat = float(request.args.get("lat"))
     long = float(request.args.get("long"))
     depth = int(request.args.get("depth"))
-    # creatures = location_animal_data.getLocationAnimalData(lat=lat, long=long)
+    creatures = []
 
-    return render_template("divedeeper.html", lat=lat, long=long, dep=depth)
+    return render_template("divedeeper.html", lat=lat, long=long, dep=depth, creatures=creatures)
 
 if __name__ == "__main__":
     index()
